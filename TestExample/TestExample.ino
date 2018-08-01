b64178e96161963a7d8444a136775561c41d76aa
LilyGO <31160602+LilyGO@users.noreply.github.com>
2018-08-01 18:06:47 +0800

    first commit

---
+++ TestExample/TestExample.ino
@@ -0,0 +1,156 @@
+
+#include <GxEPD.h>
+#include <GxGDEH029A1/GxGDEH029A1.cpp>      // 2.9" b/w
+#include GxEPD_BitmapExamples
+// FreeFonts from Adafruit_GFX
+#include <Fonts/FreeMonoBold9pt7b.h>
+#include <Fonts/FreeMonoBold12pt7b.h>
+#include <Fonts/FreeMonoBold18pt7b.h>
+#include <Fonts/FreeMonoBold24pt7b.h>
+#include <GxIO/GxIO_SPI/GxIO_SPI.cpp>
+#include <GxIO/GxIO.cpp>
+// GxIO_SPI(SPIClass& spi, int8_t cs, int8_t dc, int8_t rst = -1, int8_t bl = -1);
+GxIO_Class io(SPI, SS, 17, 16); // arbitrary selection of 17, 16
+// GxGDEP015OC1(GxIO& io, uint8_t rst = D4, uint8_t busy = D2);
+GxEPD_Class display(io, 16, 4); // arbitrary selection of (16), 4
+
+void setup()
+{
+  Serial.begin(115200);
+  Serial.println();
+  Serial.println("setup");
+
+  display.init();
+
+  Serial.println("setup done");
+}
+
+void loop()
+{
+  showBitmapExample();
+  delay(2000);
+#if !defined(__AVR)
+  drawCornerTest();
+  showFont("FreeMonoBold9pt7b", &FreeMonoBold9pt7b);
+  showFont("FreeMonoBold12pt7b", &FreeMonoBold12pt7b);
+  //showFont("FreeMonoBold18pt7b", &FreeMonoBold18pt7b);
+  //showFont("FreeMonoBold24pt7b", &FreeMonoBold24pt7b);
+#else
+  display.drawCornerTest();
+  delay(2000);
+  display.drawPaged(showFontCallback);
+#endif
+  delay(10000);
+}
+
+//DASDAS
+#if defined(_GxGDEH029A1_H_)
+void showBitmapExample()
+{
+//  display.drawExampleBitmap(BitmapExample1, sizeof(BitmapExample1));
+//  delay(2000);
+//  display.drawExampleBitmap(BitmapExample2, sizeof(BitmapExample2));
+//  delay(5000);
+//  display.fillScreen(GxEPD_WHITE);
+//  display.drawExampleBitmap(BitmapExample1, 0, 0, GxEPD_WIDTH, GxEPD_HEIGHT, GxEPD_BLACK);
+//  display.update();
+//  delay(5000);
+  showBoat();
+}
+#endif
+void showFont(const char name[], const GFXfont* f)
+{
+  display.fillScreen(GxEPD_WHITE);
+  display.setTextColor(GxEPD_BLACK);
+  display.setFont(f);
+  display.setCursor(0, 0);
+  display.println();
+  display.println(name);
+  display.println(" !\"#$%&'()*+,-./");
+  display.println("0123456789:;<=>?");
+  display.println("@ABCDEFGHIJKLMNO");
+  display.println("PQRSTUVWXYZ[\\]^_");
+#if defined(HAS_RED_COLOR)
+  display.setTextColor(GxEPD_RED);
+#endif
+  display.println("`abcdefghijklmno");
+  display.println("pqrstuvwxyz{|}~ ");
+  display.update();
+  delay(5000);
+}
+
+void showFontCallback()
+{
+  const char* name = "FreeMonoBold9pt7b";
+  const GFXfont* f = &FreeMonoBold9pt7b;
+  display.fillScreen(GxEPD_WHITE);
+  display.setTextColor(GxEPD_BLACK);
+  display.setFont(f);
+  display.setCursor(0, 0);
+  display.println();
+  display.println(name);
+  display.println(" !\"#$%&'()*+,-./");
+  display.println("0123456789:;<=>?");
+  display.println("@ABCDEFGHIJKLMNO");
+  display.println("PQRSTUVWXYZ[\\]^_");
+#if defined(HAS_RED_COLOR)
+  display.setTextColor(GxEPD_RED);
+#endif
+  display.println("`abcdefghijklmno");
+  display.println("pqrstuvwxyz{|}~ ");
+}
+
+void drawCornerTest()
+{
+  display.drawCornerTest();
+  delay(5000);
+  uint8_t rotation = display.getRotation();
+  for (uint16_t r = 0; r < 4; r++)
+  {
+    display.setRotation(r);
+    display.fillScreen(GxEPD_WHITE);
+    display.fillRect(0, 0, 8, 8, GxEPD_BLACK);
+    display.fillRect(display.width() - 18, 0, 16, 16, GxEPD_BLACK);
+    display.fillRect(display.width() - 25, display.height() - 25, 24, 24, GxEPD_BLACK);
+    display.fillRect(0, display.height() - 33, 32, 32, GxEPD_BLACK);
+    display.update();
+    delay(5000);
+  }
+  display.setRotation(rotation); // restore
+}
+
+
+#if defined(_GxGDEP015OC1_H_) || defined(_GxGDE0213B1_H_) || defined(_GxGDEH029A1_H_)
+#include "IMG_0001.h"
+void showBoat()
+{
+  // thanks to bytecrusher: http://forum.arduino.cc/index.php?topic=487007.msg3367378#msg3367378
+  uint16_t x = (display.width() - 64) / 2;
+  uint16_t y = 5;
+  display.fillScreen(GxEPD_WHITE);
+  display.drawExampleBitmap(gImage_IMG_0001, x, y, 64, 180, GxEPD_BLACK);
+  display.update();
+  delay(500);
+  uint16_t forward = GxEPD::bm_invert | GxEPD::bm_flip_x;
+  uint16_t reverse = GxEPD::bm_invert | GxEPD::bm_flip_x | GxEPD::bm_flip_y;
+  for (; y + 180 + 5 <= display.height(); y += 5)
+  {
+    display.fillScreen(GxEPD_WHITE);
+    display.drawExampleBitmap(gImage_IMG_0001, x, y, 64, 180, GxEPD_BLACK, forward);
+    display.updateWindow(0, 0, display.width(), display.height());
+    delay(500);
+  }
+  delay(1000);
+  for (; y >= 5; y -= 5)
+  {
+    display.fillScreen(GxEPD_WHITE);
+    display.drawExampleBitmap(gImage_IMG_0001, x, y, 64, 180, GxEPD_BLACK, reverse);
+    display.updateWindow(0, 0, display.width(), display.height());
+    delay(1000);
+  }
+  display.update();
+  delay(1000);
+}
+#endif
+
+
